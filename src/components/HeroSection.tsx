828e3e275cecb967036997b72fbbe228331ad201
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-11-06 00:21:44 +0500

    responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,20 +7,20 @@ const fontLibreBodoni = Libre_Bodoni({ weight: '700', subsets: ['latin'] });
 
 const HeroSection = () => {
     return (
-        <div className='flex items-center justify-between px-16 py-12'>
+        <div className='flex flex-col md:flex-row items-center justify-between px-4 md:px-16 py-8 md:py-12 gap-8'>
 
-            <div className="w-[496px] h-[189px] p-4 flex flex-col gap-10 justify-center">
-                <h1 className={`${fontLibreBodoni.className} font-weight-[700] text-[40px] leading-[65.8px] tracking-[2.5%] text-[#000000] top-[316px] left-[176px]`}>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h1>
-                <p className={`${fontLibreBodoni.className} w-[902px] h-[147px] font-weight-[500] text-[30px] leading-[49.35px] tracking-[2.5%] text-[#787054] `}>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our <br /> desirable collection.</p>
+            <div className="flex-1 flex flex-col gap-6 md:gap-10 text-center md:text-left max-w-xl">
+                <h1 className={`${fontLibreBodoni.className} font-weight-[700] text-[40px] leading-[65.8px] tracking-[2.5%] text-[#000000] top-[316px] left-[176px]  md:text-4xl lg:text-5xl`}>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h1>
+                <p className={`${fontLibreBodoni.className} text-lg md:text-xl lg:text-2xl text-[#787054] `}>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
                 
-                <div className='w-[247.67px] h-[49.2px] radius-[8.6px] gap-[8.6px] bg-[#A29875] p-[8.6px] top-[710.32px] left-[107.49px] rounded-lg text-center  '>
-                    <button className={`${fontLibreBodoni.className} font-weight-[500] text-[25.8px] tracking-[-1px] text-[#FFFFFF] w-[153px] h-[32px] `}>Explore Now</button>
+                <div className='mt-4 '>
+                    <button className={`${fontLibreBodoni.className} bg-[#A29875] text-white py-2 px-6 rounded-md text-lg md:text-xl`}>Explore Now</button>
                 </div>
             </div>
 
-            <div className='relative '>
+            <div className='relative'>
                 <Image src={hero_section_jew} alt='hero-section' className=' w-[421px] h-[573.59px]'/>
-                <Image src={hero_layer_image} alt='hero-layer' layout='fill' objectFit='cover' className='absolute top-[276.91px] left-[977.77px] pointer-events-none p-[12px] border-[#FFFFFF] rounded-t-full-[128.99px] rounded-b-full-[128.99px] w-[380.1px] h-[525.43px]' />
+                <Image src={hero_layer_image} alt='hero-layer' layout='fill' objectFit='cover' className='absolute top-[256.91px] left-[877.77px] pointer-events-none p-[12px] border-[#FFFFFF] rounded-t-full-[128.99px] rounded-b-full-[128.99px] w-[380.1px] h-[525.43px] md:h-[525px] md:w-[380px]' />
             </div>
 
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,78 @@
+"use client";
 import Image from 'next/image';
 import heart_image from '../public/images/heart.png';
 import profile_image from '../public/images/profile.png';
-import cart_icon from   '../public/images/cart.png';
-
+import cart_icon from '../public/images/cart.png';
 import { Rye } from '@next/font/google';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 const fontRye = Rye({ weight: '400', subsets: ['latin'] });
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
     return (
 
         <div className='w-full h-full'>
             <nav className="navbar bg-[#A29875] w-auto h-auto flex items-center justify-between pr-9 ">
-
-                <h1 className={`${fontRye.className} text-[65px] leading-[93.75px] text-[#FFFFFF] h-[94px] w-[439px] pt-[6px] pl-[36px] md:leading-snug`}>MANZZARI</h1>
-                <div className='flex items-center space-x-9'>
-                <div className='h-10 w-[700px] bg-[#F8F8F8] flex items-center rounded-xl px-9 '>
-                    <input type="text" className="h-full w-full bg-[#f8f8f8] focus:outline-none" placeholder="Search for Gold Jewellery, Diamond Jewellery and more..." />
-                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            
+                <h1 className={`${fontRye.className} text-3xl text-[#FFFFFF] md:text-[65px] md:leading-[93.75px]  pl-[36px] pt-[6px] `}>MANZZARI</h1>
+                <div className='hidden md:flex items-center space-x-9'>
+                    <div className='h-10 w-[700px] bg-[#F8F8F8] flex items-center rounded-xl px-9 '>
+                        <input type="text" className="h-full w-full bg-[#f8f8f8] focus:outline-none" placeholder="Search for Gold Jewellery, Diamond Jewellery and more..." />
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                    </svg>
+                        </svg>
 
-                </div>
-                <div className='flex gap-4 items-center'>
-                        <Image src={heart_image} alt='heart' className='w-[33.42px] h-[29.71px]'/>
-                        <Image src={profile_image} alt='profile' className='w-[28.97px] h-[29.26px]'  />
-                        <Image src={cart_icon} alt='cart' className='w-[38.01px] h-[30.08px]'/>
+                    </div>
+                    <div className='flex gap-4 items-center'>
+                        <Image src={heart_image} alt='heart' className='w-[33.42px] h-[29.71px]' />
+                        <Image src={profile_image} alt='profile' className='w-[28.97px] h-[29.26px]' />
+                        <Image src={cart_icon} alt='cart' className='w-[38.01px] h-[30.08px]' />
 
                     </div>
                 </div>
+                <RxHamburgerMenu onClick={() => setOpen(!open)} className='text-[#0c0c0c] w-8 h-8 cursor-pointer md:hidden' />
             </nav>
+            {open && (
+                <div className="md:hidden z-10 fixed top-0 left-0 w-full h-full bg-gray-400/10 backdrop-blur-xl flex flex-col items-center p-4">
+                    <div className="flex justify-between w-full mb-8">
+                        <h1 className={`${fontRye.className} text-3xl text-[#FFFFFF]`}>
+                            MANZZARI
+                        </h1>
+                        <button onClick={() => setOpen(false)} className="text-[#080808]">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="w-full flex flex-col items-center space-y-4">
+                        <div className="h-10 w-full bg-[#F8F8F8] flex items-center rounded-xl px-4">
+                            <input
+                                type="text"
+                                className="h-full w-full bg-[#f8f8f8] focus:outline-none"
+                                placeholder="Search for Gold Jewellery, Diamond Jewellery and more..." />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            </svg>
+                        </div>
+                        <div className="flex gap-4 items-center">
+                        <Image src={heart_image} alt='heart' className='w-[33.42px] h-[29.71px]' />
+                        <Image src={profile_image} alt='profile' className='w-[28.97px] h-[29.26px]' />
+                        <Image src={cart_icon} alt='cart' className='w-[38.01px] h-[30.08px]' />
+                        </div>
+                    </div>
+                </div>
+
+            )}
+
         </div>
     );
 }
